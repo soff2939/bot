ef216cf20187412704cffc07db956ae8aaf9240b
sofia espinosa <soff.espinosa39@gmail.com>
2025-04-16 22:22:07 -0600

    Actualizar modelo de manejo de imagenes

--- main.py
+++ main.py
@@ -1,25 +1,43 @@
 import discord
 from discord.ext import commands
-import google.generativeai as genai
+from model import get_class
+import os, random
+import requests
+
 intents = discord.Intents.default()
 intents.message_content = True
-bot = commands.Bot(command_prefix='!', intents=intents)
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
 @bot.event
 async def on_ready():
     print(f'We have logged in as {bot.user}')
+
+def get_duck_image_url():
+    url = 'https://random-d.uk/api/random'
+    res = requests.get(url)
+    data = res.json()
+    return data['url']
+
+
+@bot.command('duck')
+async def duck(ctx):
+    '''The duck command returns the photo of the duck'''
+    print('hello')
+    image_url = get_duck_image_url()
+    await ctx.send(image_url)
+
+
 @bot.command()
-async def hello(ctx):
-    await ctx.send(f'Hi! I am a bot {bot.user}!')
-@bot.command()
-async def heh(ctx, count_heh = 5):
-    await ctx.send("he" * count_heh)
-genai.configure(api_key="AIzaSyBIRjbs_w7graezF9xzG5GK92F-z6cf7gc")
-# Modelo Gemini
-model = genai.GenerativeModel("gemini-1.5-flash")
-@bot.command()
-async def gemini(ctx, *, pregunta: str):
-    """Comando para preguntar a Gemini"""
-    await ctx.send(" Pensando...")
-    response = model.generate_content(pregunta)
-    await ctx.send(response.text)
-bot.run("MTI5NDg2NDM1NjYxNzc0ODUwMA.G4kOt2.P0HCy2FlM3TKFhqznqNY2sslrp5dYu1IVzUlKI")
+async def check(ctx):
+    if ctx.message.attachments:
+        for attachment in ctx.message.attachments:
+            file_name = attachment.filename
+            file_url = attachment.url
+            await attachment.save(f"./{attachment.filename}")
+            await ctx.send(get_class(model_path="./keras_model.h5", labels_path="labels.txt", image_path=f"./{attachment.filename}"))
+    else:
+        await ctx.send("You forgot to upload the image :(")
+
+
+bot.run('TOKEN')
